0b3aa4f762ffe786e10b0405bb953997b5686d21
Joao Galeao <jbgaleao@gmail.com>
2022-06-28 01:45:15 -0300

    Instalado Entity Framework Core 3.1.8 25:00

---
+++ CL.WebApi/WebApplication1/Controllers/ClientesController.cs
@@ -0,0 +1,65 @@
+﻿using CL.Core.Domain;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CL.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        // GET: api/<ClientesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<Cliente>()
+            {
+                new Cliente
+                {
+                    Id=1,
+                    Nome="João",
+                    DataNascimento=new DateTime(1989, 02, 15)
+                },
+
+                new Cliente
+                {
+                    Id=2,
+                    Nome="Geana",
+                    DataNascimento=new DateTime(1967, 05, 14)
+                }
+            });
+        }
+
+        // GET api/<ClientesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ClientesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ClientesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ClientesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- CL.WebApi/WebApplication1/Startup.cs
+++ CL.WebApi/WebApplication1/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 using System;
 using System.Collections.Generic;
@@ -27,6 +28,14 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Consultorio Legal",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +45,13 @@ namespace WebApplication1
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.RoutePrefix = string.Empty;
+                s.SwaggerEndpoint("./swagger/v1/swagger.json", "CL V1");
+            });
 
             app.UseHttpsRedirection();
 
